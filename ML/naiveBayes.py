0bf563f08d65a5071130d4d2dfbd5a2f9c5a8c8b
doobaryreza@gmail.com <doobaryreza@gmail.com>
2020-06-10 18:14:53 +0100

    Multinomial NB added with tests

--- ML/naiveBayes.py
+++ ML/naiveBayes.py
@@ -1,5 +1,6 @@
 from scipy.stats import multivariate_normal
 import numpy as np
+from collections import Counter
 
 
 class NaiveBayes:
@@ -75,3 +76,59 @@ class GaussianNaiveBayes(NaiveBayes):
     
     def get_gaussian_data_per_feature(self, i):
         return self.data[i]
+
+class MultiNomialNaiveBayes(NaiveBayes):
+    """
+    This is just the categorical NB.
+    """
+    def __init__(self):
+        pass
+    
+    def setUp(self, X, y):
+        self.classes = np.unique(y)
+        self.K = len(self.classes)
+        
+        self.X = X
+        self.y = y
+        
+        self.N, self.p = self.X.shape
+    
+    def _find_distribution_data(self):
+        
+        self.classes = np.sort(np.unique(self.y.T[0]))
+
+        self.probs = {}
+        self.PCk = {}
+        for k in self.classes:
+            self.classkdata = self.X[np.where(self.y.T[0] == k)].T
+            self.probs[k] = {i:Counter(x) for i,x in enumerate(self.classkdata)}
+            nk = self.classkdata.shape[1]
+            self.PCk[k] = nk
+            for key in self.probs[k]:
+                for counter in self.probs[k][key]:
+                    self.probs[k][key][counter]/=nk
+                    
+    
+    def fit(self, X, y):
+        self.setUp(X, y)
+        self._find_distribution_data()
+        
+    def predict_proba(self, X):
+        N,p = X.shape
+        res = []
+        for k in self.classes:
+            pro = np.prod(np.array([list(map(lambda x: self.probs[k][i][x], X[:,i])) for  i in range(0,self.p)]).T,1)
+            pro*=self.PCk[k]
+            res.append(pro)
+        res=np.array(res)
+        res = res.T
+        predict_proba = res/res.sum(1).reshape(N,1)
+        return predict_proba
+    
+    def predict(self, X):
+        result = self.predict_proba(X)
+        prediction = np.argmax(result, 1)
+        return prediction
+    
+    def get_multinomial_data_per_class(self, k):
+        return self.probs[k]
